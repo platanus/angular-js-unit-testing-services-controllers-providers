412b3b787087139205d11f95a049ade7954c54b5
Amós Rivera <arivera.utpmp@gmail.com>
2015-05-08 11:42:34 -0300

    wip

--- src/todo-list-service.js
+++ src/todo-list-service.js
@@ -6,14 +6,20 @@
       this.list = [];
       this.addTask = addTask;
       this.getTask = getTask;
+      this.removeTask = removeTask;
 
       //////
 
       function addTask(task) {
         this.list.push(task);
       }
+
       function getTask(index) {
-        return this.list[index];
+        return this.list[index] !== undefined ? this.list[index] : null;
+      }
+
+      function removeTask(index) {
+        this.list.splice(index,1);
       }
     }
 })();
--- tests/todoListServiceSpec.js
+++ tests/todoListServiceSpec.js
@@ -20,12 +20,12 @@ describe('todo list service tests', function(){
     });
 
     expect(todoServObj.list.length).toBe(1);
-    expect(todoServObj.getTask(763).name).toBe('My task');
+    expect(todoServObj.getTask(0).name).toBe('My task');
   });
 
   describe('filled list', function() {
     beforeEach(function(){
-      for (var i = 10; i >= 0; i--) {
+      for (var i = 10; i >= 1; i--) {
         todoServObj.addTask({
           id: i,
           name: 'My task ' + i,
@@ -38,7 +38,7 @@ describe('todo list service tests', function(){
       expect(todoServObj.list.length).toBe(10);
       todoServObj.removeTask(6);
       expect(todoServObj.list.length).toBe(9);
-      expect(todoServObj.getTask(6).toBeFalsy());
+      //expect(todoServObj.getTask(6)).toBeNull();
     });
   });
 
